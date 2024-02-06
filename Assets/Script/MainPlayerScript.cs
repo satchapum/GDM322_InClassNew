4f47e22ff34857ad8756f60795a4cfe59b87b7b8
satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-02-06 10:25:03 +0700

    ADD: Name on head

--- Assets/Script/MainPlayerScript.cs
+++ Assets/Script/MainPlayerScript.cs
@@ -2,13 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class MainPlayerScript : NetworkBehaviour
 {
     public float speed = 5.0f;
     public float rotationSpeed = 10.0f;
     Rigidbody rb;
+    public TMP_Text namePrefab;
+    private TMP_Text nameLabel;
+
+    public override void OnNetworkSpawn()
+    {
+        GameObject canvas = GameObject.FindWithTag("MainCanvas");
+        nameLabel = Instantiate(namePrefab, Vector3.zero,Quaternion.identity) as TMP_Text;
+        nameLabel.transform.SetParent(canvas.transform);
+    }
     // Start is called before the first frame update
+
+    private void Update()
+    {
+        Vector3 nameLabelPos = Camera.main.WorldToScreenPoint(transform.position + new Vector3(0, 2.5f, 0));
+        nameLabel.text = gameObject.name;
+        nameLabel.transform.position = nameLabelPos;
+    }
+
+    private void OnDestroy()
+    {
+        if (nameLabel != null) Destroy(nameLabel.gameObject);
+        base.OnDestroy();
+    }
+
     void Start()
     {
         rb = this.GetComponent<Rigidbody>();
